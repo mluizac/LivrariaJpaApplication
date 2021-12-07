dcdbd6fbc260475953e93460a9ab869b77992866
Maria Luiza de Oliveira <marialuizac.oliveira@gmail.com>
2021-12-07 17:42:49 -0300

    Adiconando as classes DTO

---
+++ src/main/java/com/LivrariaJpaApplication/dto/EditoraDto.java
@@ -0,0 +1,10 @@
+package com.LivrariaJpaApplication.dto;
+
+import com.LivrariaJpaApplication.model.Editora;
+
+public class EditoraDto extends Editora {
+
+    private Long livro;
+    private String nome;
+
+}
---
+++ src/main/java/com/LivrariaJpaApplication/dto/LivroDto.java
@@ -0,0 +1,13 @@
+package com.LivrariaJpaApplication.dto;
+
+import com.LivrariaJpaApplication.model.Livro;
+
+public class LivroDto extends Livro {
+
+    private String editora;
+    private String autores;
+    private Long codigoLivro;
+    private String titulo;
+    private Long ano;
+
+}
